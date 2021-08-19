4c6c3dcc87e8ec66a227f7578be37179134ef44e
r4k4210 <elchopi26@gmail.com>
2021-08-19 12:22:11 -0300

    v1.0.1

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.ts",
+    libraryTarget: "commonjs2",
   },
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
