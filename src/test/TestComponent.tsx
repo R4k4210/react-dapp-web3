42491b01309f640deeaf8ce06d391f4b81dd4cb4
r4k4210 <elchopi26@gmail.com>
2021-08-26 09:39:12 -0300

    WIP with test

---
+++ src/test/TestComponent.tsx
@@ -0,0 +1,22 @@
+import useWeb3 from "../hooks";
+
+const TestComponent = () => {
+  const {
+    isWalletConnected,
+    walletAddress,
+    signature,
+    connect,
+    disconnect,
+    signMessage,
+  } = useWeb3();
+
+  return (
+    <div>
+      <span id="wallet-address">{walletAddress}</span>
+      <span id="signature">{signature}</span>
+      <span id="is-connected">{isWalletConnected}</span>
+    </div>
+  );
+};
+
+export default TestComponent;
--- src/test/example.test.tsx
+++ src/test/example.test.tsx
@@ -1,17 +1,54 @@
+import renderer from "react-test-renderer";
+import ShallowRenderer from "react-test-renderer/shallow";
 import withWeb3 from "../hoc";
-
-beforeEach(() => {
-  jest.resetModules();
-  process.env = Object.assign(process.env, {
-    REACT_APP_INFURA_ID: "6652e2b820d540eb8e855da9757c08f4",
-  });
-});
+import useWeb3 from "../hooks";
+import Web3 from "web3";
+import TestComponent from "./TestComponent";
 
 //Run command: NODE_OPTIONS=--unhandled-rejections=warn yarn test
+beforeEach(() => {
+  jest.resetModules();
 
-describe("Render a components with hoc props", () => {
+  //process.env = Object.assign(process.env, {
+  //REACT_APP_INFURA_ID: "6652e2b820d540eb8e855da9757c08f4",
+  //});
+});
+
+describe("Render a component with props added by HOC", () => {
   test("should add web3Data prop to wrapped component", () => {
     const mockedComponent = jest.fn(() => null);
     const WrappedComponent = withWeb3(mockedComponent);
+    const renderer = ShallowRenderer.createRenderer();
+    renderer.render(<WrappedComponent />);
+    const result = renderer.getRenderOutput();
+    expect(result.props.web3Data).toBeDefined();
+    expect(result.props.web3Data.isWalletConnected).toBeFalsy();
+    expect(result.props.web3Data.walletAddress).toBe("");
+    expect(result.props.web3Data.signature).toBe("");
+    expect(result.props.web3Data.web3).toBeNull();
+    expect(result.props.web3Data.connect).toBeInstanceOf(Function);
+    expect(result.props.web3Data.disconnect).toBeInstanceOf(Function);
+    expect(result.props.web3Data.signMessage).toBeInstanceOf(Function);
+  });
+});
+
+describe("Call useWeb3 hook", () => {
+  test("should be able to call connect function", () => {
+    jest.mock("../hooks"),
+      () => ({
+        useWeb3: () => ({
+          isWalletConnected: true,
+          walletAddress: "0xtest",
+          signature: "",
+          web3: new Web3(window.ethereum),
+          connect: jest.fn(),
+          disconnect: jest.fn(),
+          signMessage: jest.fn(),
+        }),
+      });
+
+    const component = renderer.create(<TestComponent />);
+    const instanceComponent = component.toJSON();
+    console.log("instance", instanceComponent);
   });
 });
