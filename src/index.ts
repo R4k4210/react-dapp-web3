5ab054b1ab93d319ab5adcf653bcb23e55288329
r4k4210 <elchopi26@gmail.com>
2021-09-02 01:58:11 -0300

    Prettified

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,4 @@ import withWeb3 from "./hoc";
 import useWeb3 from "./hooks";
 import { IUseWeb3, IWithWeb3, IWeb3Context } from "./types/types";
 
-export {
-  withWeb3,
-  useWeb3,
-  Web3Context,
-  Web3ContextProvider,
-  IUseWeb3,
-  IWithWeb3,
-  IWeb3Context,
-};
+export { withWeb3, useWeb3, Web3Context, Web3ContextProvider, IUseWeb3, IWithWeb3, IWeb3Context };
